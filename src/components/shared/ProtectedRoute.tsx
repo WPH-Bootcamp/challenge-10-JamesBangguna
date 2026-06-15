11406c20ea2ba17236d8d74adf09ae8a9439c309
JamesBangguna <jbangguna@gmail.com>
2026-06-15 23:01:47 +0800

    feat: add protected route component

---
+++ src/components/shared/ProtectedRoute.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useAuthStore } from '@/store/auth-store';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const token = useAuthStore((state) => state.token);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!token) {
+      router.push('/login');
+    }
+  }, [token, router]);
+
+  if (!token) {
+    return null;
+  }
+
+  return children;
+}
